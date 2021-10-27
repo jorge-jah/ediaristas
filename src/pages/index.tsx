79ad77ae3bbec41a45ccd7bc31e2d2798155ebc6
Jorge Jah <jorgejah14@gmai.com>
2021-10-26 23:28:43 -0300

    master

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import SafeEnviroment from 'ui/components/feedback/SafeEnviroment/SafeEnviroment'
+import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+
+const Home: NextPage = () => {
+  return (
+    
+    <div> 
+      <SafeEnviroment/>
+      <PageTitle 
+        title={"Conheça os profissionais"}
+        subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+      />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -0,0 +1,19 @@
+import { styled } from '@mui/material/styles';
+
+export const PageTitleContainer = styled('div')`
+    text-align:center;
+    margin: 40px 0;
+`;
+
+export const PageTitleStyled = styled('h2')`
+    margin: 0;
+    color: #6B2AEE;
+    font-weight: 600;
+    
+`;
+
+export const PageSubtitleStyled = styled('h3')`
+    margin: 12px 0;
+    font-weight: normal;
+
+`;
